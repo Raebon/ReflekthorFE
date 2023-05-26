f0d514c6dde2673b47c845f32577728c753af9ff
Dominik Kučera <kuceradominik123@gmail.com>
2023-05-26 12:57:46 +0200

    update

--- src/app/(profile)/dashboard/category/DataTableComponent.tsx
+++ src/app/(profile)/dashboard/category/DataTableComponent.tsx
@@ -3,11 +3,14 @@ import { FC } from "react";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { useGetCategoriesSetupQuery } from "@/utils/api/query/getCategoriesQueryKey";
+import { CategoryDto } from "@/types/reflektor-api-service";
 
-interface DataTableComponentProps {}
+interface DataTableComponentProps {
+  initialData: CategoryDto[];
+}
 
-const DataTableComponent: FC<DataTableComponentProps> = ({}) => {
-  const { data } = useGetCategoriesSetupQuery();
+const DataTableComponent: FC<DataTableComponentProps> = ({ initialData }) => {
+  const { data } = useGetCategoriesSetupQuery(initialData);
   return <DataTable columns={columns} data={data ?? []} />;
 };
 
--- src/app/(profile)/dashboard/category/page.tsx
+++ src/app/(profile)/dashboard/category/page.tsx
@@ -1,17 +1,32 @@
 import CategoryGridBannerComponent from "@/components/dashboard-page/CategoryGridBannerComponent";
 import type { Metadata } from "next";
 import DataTableComponent from "./DataTableComponent";
+import axios, { AxiosResponse } from "axios";
+import { CategoryDto } from "@/types/reflektor-api-service";
 
 export const metadata: Metadata = {
   title: "Reflektor | Categories",
   description: "Reflektor",
 };
 
-export default function Category() {
+async function getCategories(): Promise<CategoryDto[]> {
+  try {
+    const response: AxiosResponse<CategoryDto[]> = await axios.get(
+      `${process.env.API_URL}/blogs/categories`
+    );
+    return response.data;
+  } catch (error) {
+    // Handle the error
+    throw error;
+  }
+}
+
+export default async function Category() {
+  const initialCategoriesData = await getCategories();
   return (
     <section>
       <CategoryGridBannerComponent />
-      <DataTableComponent />
+      <DataTableComponent initialData={initialCategoriesData} />
     </section>
   );
 }
